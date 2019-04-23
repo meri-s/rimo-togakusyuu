f57447894427498e176c7a19f2a767ac73a8fc31
meri-s <49223796+meri-s@users.noreply.github.com>
2019-04-23 16:27:41 +0900

    Update sample program.c

--- sample program.c
+++ sample program.c
@@ -1,9 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
 void main(void) {
+	/* 変数宣言 */
 	char num[5] = { 0 };
+	char str[256] = { 0 };
+	short h = 0;
 	FILE *fp = NULL;
 
 	/* 永久ループ */
@@ -12,33 +17,40 @@ void main(void) {
 		gets(&(num[0]));			/* 検索キーを入力 */
 
 		/* "EXIT"と入力するとループ終了 */
-		if (strcmp(num, "EXIT") == 0) {		/* 入力した値と"EXIT"を比べて、等しい時 0 を返し、==で判定 */
+		if (strcmp(&(num[0]), "EXIT") == 0) {		/* 入力した値と"EXIT"を比べて、等しい時 0 を返し、==で判定 */
 			break;
 		}
 
-		/* 入力した値で分岐 */
-		switch (atoi(num)) {	/* 文字列を数値に変換 */
-			case  1:	printf("[該当データ]:\n");				break;	/* 検索キー 001 の場合		*/
-			case  2:	printf("[該当データ]:\n");				break;	/* 検索キー 002 の場合		*/
-			case  3:	printf("[該当データ]:\n");				break;	/* 検索キー 003 の場合		*/
-			case  4:	printf("[該当データ]:\n");				break;	/* 検索キー 004 の場合		*/
-			case  5:	printf("[該当データ]:\n");				break;	/* 検索キー 005 の場合		*/
-			case  6:	printf("[該当データ]:\n");				break;	/* 検索キー 006 の場合		*/
-			case  7:	printf("[該当データ]:\n");				break;	/* 検索キー 007 の場合		*/
-			case  8:	printf("[該当データ]:\n");				break;	/* 検索キー 008 の場合		*/
-			case  9:	printf("[該当データ]:\n");				break;	/* 検索キー 009 の場合		*/
-			case 10:	printf("[該当データ]:\n");				break;	/* 検索キー 010 の場合		*/
-			case 11:	printf("[該当データ]:\n");				break;	/* 検索キー 011 の場合		*/
-			case 12:	printf("[該当データ]:\n");				break;	/* 検索キー 012 の場合		*/
-			case 13:	printf("[該当データ]:\n");				break;	/* 検索キー 013 の場合		*/
-			case 15:	printf("[該当データ]:\n");				break;	/* 検索キー 015 の場合		*/
-			case 16:	printf("[該当データ]:\n");				break;	/* 検索キー 016 の場合		*/
-			case 17:	printf("[該当データ]:\n");				break;	/* 検索キー 017 の場合		*/
-			case 18:	printf("[該当データ]:\n");				break;	/* 検索キー 018 の場合		*/
-			case 19:	printf("[該当データ]:\n");				break;	/* 検索キー 019 の場合		*/
-			case 20:	printf("[該当データ]:\n");				break;	/* 検索キー 020 の場合		*/
-			default:	printf("[検索結果]:該当データなし\n");	break;	/* 検索キーと合致しない場合 */
+		/* data.txtファイルオープン */
+		fp = fopen("data.txt", "r");
+
+		/* 入力した値が 001 ～ 013 の場合 */
+		if ((atoi(num) <= 13) && (atoi(num) >= 1)) {
+			for (h = atoi(num); h != 0; h--) {		/* h に入力した値を数値で代入 */	/* hが0でなければループ */	/* hをデクリメント */
+				fseek(fp, 4, SEEK_CUR);				/* ファイルの読み込み位置を4byteずらす */
+				fgets(&(str[0]), sizeof(str), fp);	/* ファイル1行分読み込み */
+			}
 		}
+
+		/* 入力した値が 015 ～ 020 の場合 */
+		if ((atoi(num) <= 20) && (atoi(num) >= 15)) {
+			for (h = atoi(num); h != 1; h--) {		/* h に入力した値を数値で代入 */	/* hが0でなければループ */	/* hをデクリメント */	
+				fseek(fp, 4, SEEK_CUR);				/* ファイルの読み込み位置を4byteずらす */
+				fgets(&(str[0]), sizeof(str), fp);	/* ファイル1行分読み込み */
+			}
+		}
+
+		/* 入力した値が 1 ～ 20 の場合 (14を除く) */
+		if ((atoi(num) >= 1) && (atoi(num) <= 20) && (atoi(num) != 14)) {	
+			printf("[該当データ]:%s", &(str[0]));
+		}
+		/* 上記以外の場合 */
+		else {																
+			printf("[検索結果]:該当データなし\n");
+		}
+
+		/* data.txtファイルクローズ */
+		fclose(fp);
 
 	}
 	return;

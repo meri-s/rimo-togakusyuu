cdf4910a4d46612b7e86625d3a3d0c70eb64e99f
meri-s <49223796+meri-s@users.noreply.github.com>
2019-04-25 17:48:11 +0900

    Update sample program.c

--- sample program.c
+++ sample program.c
@@ -1,15 +1,31 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include<stdio.h>
-#include<stdlib.h>
 #include<string.h>
 
 void main(void) {
 	/* 変数宣言 */
-	char num[5] = { 0 };
-	char str[256] = { 0 };
-	short h = 0;
+	char num[5] = { 0 };			/* 入力した値を格納する配列 */
+	char str[1000][261] = { 0 };	/* ファイルのデータを格納する二次元配列 */
 	FILE *fp = NULL;
+	char flag = 0;					/* フラグ [ON:1/OFF:0] */
+
+	/* data.txtファイルオープン */
+	fp = fopen("data.txt", "r");
+
+	/* ファイルを開けなかった場合 */
+	if (fp == NULL) {
+		printf("ファイルを開けませんでした。\n");
+		return;		/* プログラム終了 */
+	}
+
+	/* ファイルのデータを読み込み */
+	for (short count = 0;count < 1000;count++) {	/* count が1000より小さい場合ループ */
+		fgets(&(str[count][0]), 261, fp);			/* データ部を読み込み */
+	}
+
+	/* data.txtファイルクローズ */
+	fclose(fp);
 
 	/* 永久ループ */
 	while (1) {
@@ -21,40 +37,26 @@ void main(void) {
 			break;
 		}
 
-		/* data.txtファイルオープン */
-		fp = fopen("data.txt", "r");
+		/* 1000回ループ */
+		for (short count = 0;count < 1000;count++) {
 
-		/* 入力した値が 001 ～ 013 の場合 */
-		if ((atoi(num) <= 13) && (atoi(num) >= 1)) {
-			h = atoi(num);							/* 入力した値を数値で h に代入 */
-			while (h != 0) {						/* h が0でない時ループ */
-				fseek(fp, 4, SEEK_CUR);				/* ファイルの読み込み位置を4byteずらす */
-				fgets(&(str[0]), sizeof(str), fp);	/* ファイル1行分読み込み */
-				h--;								/* hをデクリメント */
+			/* 入力した値とファイルの検索部 3byte が同じとき */
+			if (strncmp(&(num[0]), &(str[count][0]), 3) == 0) {
+				printf("[該当データ]:%s", &(str[count][4]));	/* 二次元配列の先頭から4番目のアドレスを渡している */
+				flag = 1;		/* フラグ：ON */
+				break;		/* ループ終了 */
 			}
+
 		}
 
-		/* 入力した値が 015 ～ 020 の場合 */
-		if ((atoi(num) <= 20) && (atoi(num) >= 15)) {
-			h = atoi(num);							/* 入力した値を数値で h に代入 */
-			while (h != 1) {						/* h が0でない時ループ */
-				fseek(fp, 4, SEEK_CUR);				/* ファイルの読み込み位置を4byteずらす */
-				fgets(&(str[0]), sizeof(str), fp);	/* ファイル1行分読み込み */
-				h--;								/* hをデクリメント */
-			}
-		}
-
-		/* 入力した値が 1 ～ 20 の場合 (14を除く) */
-		if ((atoi(num) >= 1) && (atoi(num) <= 20) && (atoi(num) != 14)) {	
-			printf("[該当データ]:%s", &(str[0]));
-		}
-		/* 上記以外の場合 */
-		else {																
+		/* フラグ：OFFの場合 */
+		if (flag == 0) {
 			printf("[検索結果]:該当データなし\n");
 		}
-
-		/* data.txtファイルクローズ */
-		fclose(fp);
+		/* フラグ：ONの場合 */
+		else if (flag == 1) {
+			flag = 0;	/* フラグ：OFF */
+		}
 
 	}
 	return;
